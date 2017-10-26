7f2cb5df4a175a250f426441c035dd0454fa4f8e
Kevin Coppa <khc339@nyu.edu>
2017-10-26 09:43:39 -0400

    Change routes for listings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
-    resources :listings, only: [:index, :show]
   end
   root 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
