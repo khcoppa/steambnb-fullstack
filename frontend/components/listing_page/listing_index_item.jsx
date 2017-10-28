7e8d14981dd68bddd1348b37b15f9b7fa4ed5dc4
Kevin Coppa <khc339@nyu.edu>
2017-10-28 14:47:01 -0400

    Update LisingIndex style

--- frontend/components/listing_page/listing_index_item.jsx
+++ frontend/components/listing_page/listing_index_item.jsx
@@ -11,6 +11,8 @@ class ListingIndexItem extends React.Component {
       <Link to={`/listings/${this.props.listing.id}`}>
         <li className="listing-item">
           <img src={this.props.listing.image_url} />
+          {this.props.listing}
+          <h4>Sauna {this.props.listing.beds} beds</h4>
           <h3>{this.props.listing.title}</h3>
           <p>From ${this.props.listing.price} per night</p>
         </li>
