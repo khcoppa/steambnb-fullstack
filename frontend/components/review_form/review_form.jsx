86373b76d67e03cccb361f11c71aa6226fdc1a02
Kevin Coppa <khc339@nyu.edu>
2017-10-31 10:58:58 -0400

    Style review page

--- frontend/components/review_form/review_form.jsx
+++ frontend/components/review_form/review_form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ListingIndexItem from '../listing_page/listing_index_item';
 
 class ReviewForm extends React.Component {
   constructor(props){
@@ -33,83 +34,92 @@ class ReviewForm extends React.Component {
     if (this.props.listing) {
       return(
         <div className="review-form">
+          <div className="listing-div">
+            <img src={this.props.listing.image_url} />
+            <h4>Sauna {this.props.listing.beds} beds</h4>
+            <h3>{this.props.listing.title}</h3>
+            <p>From ${this.props.listing.price} per night</p>
+          </div>
+
           <form onSubmit={this.handleSubmit}>
 
             <div className="rating">
               <h2>How was your stay at {this.props.listing.title}?</h2>
-              <label>
-                <input
-                  type="radio"
-                  name="stars"
-                  value="1"
-                  onChange={this.update('rating')}
-                />
-                <span className="icon">★</span>
-              </label>
+              <div className="stars">
+                <label>
+                  <input
+                    type="radio"
+                    name="stars"
+                    value="1"
+                    onChange={this.update('rating')}
+                  />
+                  <span className="icon">★</span>
+                </label>
 
-              <label>
-                <input
-                  type="radio"
-                  name="stars"
-                  value="2"
-                  onChange={this.update('rating')}
-                />
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-              </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="stars"
+                    value="2"
+                    onChange={this.update('rating')}
+                  />
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                </label>
 
-              <label>
-                <input
-                  type="radio"
-                  name="stars"
-                  value="3"
-                  onChange={this.update('rating')}
-                />
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-              </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="stars"
+                    value="3"
+                    onChange={this.update('rating')}
+                  />
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                </label>
 
-              <label>
-                <input
-                  type="radio"
-                  name="stars"
-                  value="4"
-                  onChange={this.update('rating')}
-                />
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-              </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="stars"
+                    value="4"
+                    onChange={this.update('rating')}
+                  />
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                </label>
 
-              <label>
-                <input
-                  type="radio"
-                  name="stars"
-                  value="5"
-                  onChange={this.update('rating')}
-                />
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-                <span className="icon">★</span>
-              </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="stars"
+                    value="5"
+                    onChange={this.update('rating')}
+                  />
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                  <span className="icon">★</span>
+                </label>
+              </div>
             </div>
 
             <div className="body">
               <h2>Write a public review</h2>
               <textarea
-                rows="40"
-                cols="40"
+                rows="15"
+                cols="45"
                 placeholder="Write a public review"
                 value={this.state.body}
                 onChange={this.update('body')}
               />
             </div>
 
-            <button>Review</button>
+            <button id="review-button">Submit</button>
 
           </form>
         </div>
