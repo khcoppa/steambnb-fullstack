e1276c81388430e3fe9214c17f2965d519c060ff
Kevin Coppa <khc339@nyu.edu>
2017-11-01 16:53:17 -0400

    Correct ListingIndex ul styling

--- frontend/components/filter_bar/filter_bar.jsx
+++ frontend/components/filter_bar/filter_bar.jsx
@@ -8,24 +8,24 @@ const FilterBar = (props) => {
   return (
     <div className="filterbar">
       <div className="filter-form">
-        <input
-          type="date"
-          value={props.start_date}
-          onChange={handleChange('start_date', props.updateFilter)}
-          placeholder="Check in"
-        />
-        <input
-          type="date"
-          value={props.end_date}
-          onChange={handleChange('end_date', props.updateFilter)}
-          placeholder="Check out"
-        />
-        <input
-          type="location"
-          value={props.location}
-          onChange={handleChange('location', props.updateFilter)}
-          placeholder="location"
-        />
+        <form className="dates">
+          <label>Check in:</label>
+            <input
+              type="date"
+              value={props.start_date}
+              onChange={handleChange('start_date', props.updateFilter)}
+              placeholder="Check in"
+            />
+
+          <label>Check out:</label>
+            <input
+              type="date"
+              value={props.end_date}
+              onChange={handleChange('end_date', props.updateFilter)}
+              placeholder="Check out"
+            />
+        </form>
+
       </div>
     </div>
   );
--- frontend/components/navbar/navbar_search_container.js
+++ frontend/components/navbar/navbar_search_container.js
@@ -1,19 +1,21 @@
 import { connect } from 'react-redux';
-
+import { updateFilter } from '../../actions/filter_actions';
 import { login, signup, logout } from '../../actions/session_actions';
 import NavbarSearch from './navbar_search';
 
-const mapStateToProps = ({ session, entities }) => {
+const mapStateToProps = ({ session, entities, ui }) => {
   return {
     currentUser: session.currentUser,
-    listings: entities.listings
+    listings: entities.listings,
+    location: ui.filters.location
   };
 };
 
 const mapDispatchToProps = dispatch => ({
   login: (user) => dispatch(login(user)),
   signup: (user) => dispatch(signup(user)),
-  logout: () => dispatch(logout())
+  logout: () => dispatch(logout()),
+  updateFilter: (filter, value) => dispatch(updateFilter(filter, value))
 });
 
 export default connect(
