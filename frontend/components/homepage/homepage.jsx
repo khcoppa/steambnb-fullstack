8968bc09e899c5bb0de6e9f56376de41e9f0abe5
Kevin Coppa <khc339@nyu.edu>
2017-11-30 11:28:08 -0500

    Correct styling of homepage

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -29,26 +29,30 @@ class Homepage extends React.Component {
   render() {
     return (
       <div className="home-content">
-        <h1>Steambnb</h1>
-        <p>Book time to relax<br /> at premier spas and saunas.</p>
 
-        <div className='search-bar-home'>
-          <div className='search-icon-home'>
-            <i className="fa fa-search"></i>
-          </div>
-          <div className='search-input-home'>
-            <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                value={this.state.location_search}
-                onChange={this.update('location_search')}
-                placeholder="Anywhere"
-              />
-              <button id="search">Search</button>
-            </form>
+        <div className="text-search">
+          <h1>Steambnb</h1>
+          <p>Book time to relax<br /> at premier spas and saunas.</p>
+
+          <div className='search-bar-home'>
+            <div className='search-icon-home'>
+              <i className="fa fa-search"></i>
+            </div>
+            <div className='search-input-home'>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  value={this.state.location_search}
+                  onChange={this.update('location_search')}
+                  placeholder="Anywhere"
+                />
+                <button id="search">Search</button>
+              </form>
+            </div>
           </div>
         </div>
-        <h4>Explore Steambnb</h4>
+
+        <h5>Explore Steambnb</h5>
         <Link to={`/listings`}>
           <div className="listings-button">
             <img src="https://i.pinimg.com/originals/39/2f/bc/392fbca43c9f32ddf836903f0dbde545.jpg" />
